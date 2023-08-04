8766a53952613750a3ccf0c69cc7723c10c7d905
Angie Arango <127635145+Angiea18@users.noreply.github.com>
2023-08-03 23:35:28 -0500

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from fastapi import FastAPI 
 from fastapi import FastAPI, Query, HTTPException
+from fastapi.templating import Jinja2Templates 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
