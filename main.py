41d7d048fd46b9efc860e6e83e036774d4efa6b2
Angie Arango <127635145+Angiea18@users.noreply.github.com>
2023-08-04 09:01:50 -0500

    main.py

--- main.py
+++ main.py
@@ -115,97 +115,25 @@ def metascore(año: str):
 
     return top_juegos_metascore
 
-df_limpio = pd.read_csv('datosML.csv')
-# Asegurarse de que 'release_date' sea de tipo datetime
-df_limpio['release_date'] = pd.to_datetime(df_limpio['release_date'])
-
-def train_bagging_model(genres, metascore, year):
-    # Filtrar los registros con valores no nulos en la columna 'genres'
-    df_filtrado = df_limpio[df_limpio['genres'].notnull()]
-
-    # Paso 1: Filtrar el DataFrame según el género y la disponibilidad anticipada
-    df_filtrado = df_filtrado[(df_filtrado['genres'].apply(lambda x: genres in x if isinstance(x, list) else False))]
-
-    # Paso 2: Preparar los datos (asegúrate de haber realizado las transformaciones previas)
-    df_filtrado['year'] = df_filtrado['release_date'].dt.year  # Crear una nueva columna 'year' con el año extraído de 'release_date'
-    X = df_filtrado[['metascore', 'year']]  # Incluir las variables metascore, year y early_access
-
-    # Obtener todos los géneros únicos presentes en el DataFrame
-    all_genres = set()
-    for genre_list in df_filtrado['genres']:
-        if isinstance(genre_list, list):
-            all_genres.update(genre_list)
-    all_genres = sorted(list(all_genres))
-
-    # Crear columnas para cada género utilizando one-hot encoding con prefijo 'genre'
-    genres_encoded = pd.get_dummies(df_filtrado['genres'].apply(pd.Series).stack(), prefix='genre').groupby(level=0).sum()
-
-    # Combinar las características codificadas con la matriz X
-    X = pd.concat([X, genres_encoded], axis=1)
-
-    y = df_filtrado['price']
-
-    # Eliminar filas con valores faltantes
-    X.dropna(inplace=True)
-    y = y[X.index]
-
-    # Paso 3: Dividir los datos en conjuntos de entrenamiento y prueba
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Paso 4: Crear el modelo base de regresión lineal
-    base_model = LinearRegression()
-
-    # Paso 5: Crear el modelo de Bagging utilizando el modelo base
-    bagging_model = BaggingRegressor(base_model, n_estimators=10, random_state=42)
-
-    # Paso 6: Entrenar el modelo de Bagging con los datos de entrenamiento
-    bagging_model.fit(X_train, y_train)
-
-    # Paso 7: Realizar predicciones con el modelo de Bagging
-    y_test_pred = bagging_model.predict(X_test)
-
-    # Paso 8: Calcular el RMSE
-    rmse = mean_squared_error(y_test, y_test_pred, squared=False)
-
-    # Guardar el modelo entrenado utilizando pickle
-    with open('bagging_model.pkl', 'wb') as model_file:
-        pickle.dump(bagging_model, model_file)
-
-    return rmse
-
-# Ejemplo de uso:
-genero_elegido = 'Action'
-metascore_elegido = 85.0
-year_elegido = 2017
-
-rmse_result = train_bagging_model(genero_elegido, metascore_elegido, year_elegido)
-print("RMSE del modelo de Bagging:", rmse_result)
-
 
 # Cargar el modelo entrenado con pickle
 with open('bagging_model.pkl', 'rb') as model_file:
     bagging_model = pickle.load(model_file)
-
-# Lista de valores disponibles para el género
-available_genres = ['Action', 'Adventure', 'Casual', 'Early Access', 'Free to Play', 'Indie',
-                    'Massively Multiplayer', 'RPG', 'Racing', 'Simulation', 'Sports', 'Strategy', 'Video Production']
-
-@app.get("/")
-async def read_root():
-    return {"message": "¡Bienvenido a la API de predicciones de precios de juegos!"}
-
+    
 @app.get("/predict/")
 async def predict_price(genres: str, metascore: float, year: int):
-    # Verificar que el valor de género esté dentro de los géneros disponibles
-    genres_list = genres.split(",")
-    for genre in genres_list:
-        if genre not in available_genres:
-            raise HTTPException(status_code=422, detail=f"Invalid genre: {genre}. It should be one of the available genres.")
-
     # Crear la matriz de características para hacer la predicción
     X = [[metascore, year]]
+
+    # Obtener todos los géneros únicos presentes en el DataFrame
+    all_genres = [
+        'Action', 'Adventure', 'Casual', 'Early Access', 'Free to Play', 'Indie',
+        'Massively Multiplayer', 'RPG', 'Racing', 'Simulation', 'Sports', 'Strategy', 'Video Production'
+    ]
+
     # Agregar columnas para los géneros, todas con valor 0 (no seleccionados)
-    for genre in available_genres:
+    genres_list = genres.split(",")
+    for genre in all_genres:
         X[0].append(1 if genre in genres_list else 0)
 
     # Realizar la predicción utilizando el modelo de Bagging
